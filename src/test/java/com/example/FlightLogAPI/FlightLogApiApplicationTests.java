13db8e029e7072bdb82c360bfa8464a7d8b1d532
Scott <scott.zetterstrom@gmail.com>
2020-07-23 12:14:11 -0600

    Initial Commit

---
+++ src/test/java/com/example/FlightLogAPI/FlightLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlightLogAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
